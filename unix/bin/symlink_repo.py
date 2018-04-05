68e5d9f311a5414999e3519e93997229e312d47e
farisachugthai <farischugthai@gmail.com>
2018-04-05 08:46:44 -0400

    Refactored symlink script to improve portability.

--- unix/bin/symlink_repo.py
+++ unix/bin/symlink_repo.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python3
 # Objectives:
-# Clone my personal dotfiles to a user's home directory and then symlink to the
-# repo.
-# Assumes Linux only because currently it has only been tested on Ubuntu
-# and Termux
+# Clone dotfiles to a user's home directory and then symlink to the repo.
+# Assumes Linux only because it has only been tested on Ubuntuband Termux.
 # Maintained by Faris Chugthai
 
 import os
 import sys
 
 
+def symlink_repo(link):
+    src = os.path.join(repo, link)
+    dest = os.path.join(home, link)
+    try:
+        os.symlink(src, dest)
+    except FileExistsError:
+        if os.path.islink(dest):
+            print("Sorry but a symlink to {0} already exists".format(dest))
+        elif os.path.isfile(dest):
+            print("Sorry but a file to {0} already exists".format(dest))
+
+
 def main():
     if os.uname()[0] != 'Linux':
-        print("This script assumes a Linux operating system. I'm sorry but the\
-        script will terminate now.")
+        raise OSError("This script assumes a Linux operating system.")
         sys.exit()
 
     os.chdir(repo)
     for root, dirs, files in os.walk("."):
         rel_paths = [(os.path.join(root, file)) for file in files]
         for link in rel_paths:
-            try:
-                os.symlink(os.path.join(repo, link), os.path.join(home, link))
-            except FileExistsError:
-                if os.path.islink(os.path.join(home, link)):
-                    print("Sorry but a symlink to {0} already exists".format(os.path.join(home, link)))
-                elif os.path.isfile(os.path.join(home, link)):
-                    print("Sorry but a file to {0} already exists".format(os.path.join(home, link)))
+            symlink_repo(link)
 
 
 if __name__ == '__main__':
     home = os.path.join(os.path.expanduser("~"), "")
-    # TODO: cpuld wrap in a try/except with expanduser(%HOME%)
+    # TODO: could wrap in a try/except with expanduser(%HOME%)
     repo = os.path.join(home, "projects", "dotfiles", "unix", "")
     main()
     os.chdir(home)
-    # TODO: Could refactor the entire try block into a function
-    # TODO: Also could make a function that takes dirs from os.walk and
+    # TODO: Make a function that takes dirs from os.walk and
     # uses os.mkdir(dirs) to create them. should be a 1 line function
     # or 2 if you want console output
