4856750b0865518121090a34c91873a24d68345b
farisachugthai <farischugthai@gmail.com>
2018-04-11 20:14:04 -0400

    Rewrote symlink_repo.py in it's entirety. Refactored global variables to ensure values are accessible if name != __main__ Added expressions in main() that create directories if they don't exist. Cleaned up how symlinks are created for readability.

--- unix/bin/symlink_repo.py
+++ unix/bin/symlink_repo.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+# Maintainer: Faris Chugthai
 
 import os
 import sys
 
+home = os.path.join(os.path.expanduser("~"), "")
+# TODO: could wrap in a try/except with expanduser(%HOME%)
+repo = os.path.join(home, "projects", "dotfiles", "unix", "")
 
-def symlink_repo(link):
+
+def symlink_repo(file):
     '''
     Symlink the dotfiles if nothing exists in the home directory.
     '''
 
-    src = os.path.join(repo, link)
-    dest = os.path.join(home, link)
+    src = os.path.join(repo, file)
+    dest = os.path.join(home, file)
     try:
         os.symlink(src, dest)
     except FileExistsError:
@@ -18,6 +23,7 @@ def symlink_repo(link):
             print("Sorry but a symlink to {0} already exists".format(dest))
         elif os.path.isfile(dest):
             print("Sorry but a file to {0} already exists".format(dest))
+            # TODO: Offer to backup the file and then proceed
 
 
 def main():
@@ -31,19 +37,16 @@ def main():
         raise OSError("This script assumes a Linux operating system.")
         sys.exit()
 
-    os.chdir(repo)
-    for root, dirs, files in os.walk("."):
-        rel_paths = [(os.path.join(root, file)) for file in files]
-        for link in rel_paths:
-            symlink_repo(link)
+    for root, dirs, files in os.walk(repo):
+        # Now lets do the folder check
+        if not os.path.isdir(root):
+            os.makedirs(root, exist_ok=False)
+
+        # Call for every file in the repo to be symlinked
+        rel_file = [os.path.join(root, file) for file in files]
+        for rel_file in files:
+            symlink_repo(rel_file)
 
 
 if __name__ == '__main__':
-    home = os.path.join(os.path.expanduser("~"), "")
-    # TODO: could wrap in a try/except with expanduser(%HOME%)
-    repo = os.path.join(home, "projects", "dotfiles", "unix", "")
     main()
-    os.chdir(home)
-    # TODO: Make a function that takes dirs from os.walk and
-    # uses os.mkdir(dirs) to create them. should be a 1 line function
-    # or 2 if you want console output
