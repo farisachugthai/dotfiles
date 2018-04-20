ec28fa9d0a18886af5b719a28f027e79e044a2c5
farisachugthai <farischugthai@gmail.com>
2018-04-20 06:35:22 -0400

    Renamed and added comments.

--- unix/bin/root_ubuntu_install.sh
+++ unix/bin/root_ubuntu_install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# This script will install the bare minimum number of packages required for
-# successful installation of all the dotfiles and a automatically generated
-# development environment
+# Maintainer: Faris Chugthai
 #
-#Objectives: Install the minimum number of packages required to create a
+# Objectives: Install the minimum number of packages required to create a
 # working python development environment
 # In addition, get the newest versions of each package.
 #
-# This script does assume a user with installation permissions
-# Maintained by Faris Chugthai
+# This script assumes a user with installation permissions
 
 apt update && apt upgrade -y && apt autoremove -y
 
@@ -24,11 +21,11 @@ apt update
 apt install vim-gtk3
 
 # Then install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install pip and conda.
 # Currently independent of x86 or x64 however still assumes Linux
 apt install python3-pip
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-`uname -m`.sh
+# wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-`uname -m`.sh
 
 # TODO: Add in modifications so that downloading Conda is completely automated
