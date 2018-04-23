1e55da254129a82d16f61ae963b5fa201709e889
farisachugthai <farischugthai@gmail.com>
2018-04-23 07:05:07 -0400

    Features, bug fixes, and formatting: Added a skeleton template for python files. Modified the behavior of the template to ensure correct format. Added a function to automatically insert the date in a LAST MODIFIED: line Add colons to accentuate headers in vimrc. Add more whitespace to clearly distinguish sections.

---
+++ unix/.vim/skeleton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==============================================================================
+#
+#          FILE: test.py
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#       CREATED: ___
+# LAST MODIFIED: 2018 Apr 23
+#    MAINTAINER: Faris Chugthai
+# ==============================================================================
+
+
+def main():
+    """docstring for main"""
+
+
+pass
