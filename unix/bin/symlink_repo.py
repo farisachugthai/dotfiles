2916d556c4b2e92a9a7d727bea6568da47f81a8b
farisachugthai <farischugthai@gmail.com>
2018-04-24 06:18:13 -0400

    Bug fixes. symlink_repo.py now correctly traverses the directories in the repo while symlinking.

--- unix/bin/symlink_repo.py
+++ unix/bin/symlink_repo.py
@@ -5,7 +5,6 @@ import os
 import sys
 
 home = os.path.join(os.path.expanduser("~"), "")
-# TODO: could wrap in a try/except with expanduser(%HOME%)
 repo = os.path.join(home, "projects", "dotfiles", "unix", "")
 
 
@@ -17,7 +16,7 @@ def symlink_repo(file):
         file --> A relative pathname that starts at the base of the Unix folder
         in the repo
 
-    Output:
+    Returns:
         None
     '''
 
@@ -30,29 +29,34 @@ def symlink_repo(file):
             print("Sorry but a symlink to {0} already exists".format(dest))
         elif os.path.isfile(dest):
             print("Sorry but a file to {0} already exists".format(dest))
-            # TODO: Offer to backup the file and then proceed
+            # TODO: Verbosely backup the file and proceed
+            # Use similar logic from backup-nt-posix.py
 
 
 def main():
     '''
     Clone dotfiles to a user's home directory and then symlink to the repo.
-    Assumes Linux only because it has only been tested on Ubuntu and Termux.
     Maintained by Faris Chugthai
     '''
 
-    if os.uname()[0] != 'Linux':
-        raise OSError("This script assumes a Linux operating system.")
+    if os.uname()[0] not in ["Darwin", "Linux"]:
+        raise OSError("This script assumes a Unix operating system.")
         sys.exit()
 
-    for root, dirs, files in os.walk(repo):
-        # Now lets do the folder check
+    # Ensure all required dirs exist and if not create them
+    cwd = os.getcwd()
+    os.chdir(repo)
+    for root, dirs, files in os.walk("."):
+        # The "." path separator gets prepended to the link so splice it
+        root = root[2:]
         if not os.path.isdir(root):
-            os.makedirs(root, exist_ok=False)
+            os.makedirs(root, exist_ok=True)
 
         # Call for every file in the repo to be symlinked
-        rel_file = [os.path.join(root, file) for file in files]
-        for rel_file in files:
-            symlink_repo(rel_file)
+        for rel_pathname in files:
+            symlink_repo(os.path.join(root, rel_pathname))
+
+    os.chdir(cwd)
 
 
 if __name__ == '__main__':
