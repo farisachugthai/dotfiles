6163bf87775da57fa5c81940ca2fe8a66021aa5b
farisachugthai <farischugthai@gmail.com>
2018-04-30 06:42:09 -0400

    mv_to_repo.py finished! Many modifications made but currently works as expected.

--- unix/bin/mv_to_repo.py
+++ unix/bin/mv_to_repo.py
@@ -8,7 +8,7 @@
 #  REQUIREMENTS: python3.4+
 #          BUGS: ---
 #       CREATED: 2018 Apr 27
-# LAST MODIFIED: 2018 Apr 29
+# LAST MODIFIED: 2018 Apr 30
 #    MAINTAINER: Faris Chugthai
 # ==============================================================================
 
@@ -31,23 +31,30 @@ def main(rel_file):
         move. As a result a pathname cannot be given to a file.
     """
 
-    shutil.copy(rel_file, str(rel_file) + ".bak")
+    shutil.copy(src, str(src) + ".bak")
     # TODO: Check that the directory exists in the repo and if not make one!
-    shutil.move(rel_file, str(repo) + filename)
-    rel_file.symlink_to(repo + rel_file)
+    dest = Path.joinpath(repo, rel_path)
+    if dest.is_dir() is not True:
+        pass
+
+    # Didnt realize this. shutil requires strings as inputs because it utilizes rstrip to return files
+    shutil.move(str(src), str(dest))
+    dest_file = Path.joinpath(dest, sys.argv[1])
+    src.symlink_to(dest_file)
 
 
 if __name__ == '__main__':
 
-    filename = sys.argv(1) if len(sys.argv) else print("Takes one filename.")
-    f = Path(filename) if filename.is_file else print("This is not a file.")
-    # TODO: Figure out how to evaluate multiple statements on one else.
-    # We also need to sys.exit() if filename.is_file is False.
+    inputted = sys.argv[1] if len(sys.argv) else print("Takes one filename.")
+    src = Path(inputted)
+    if src.is_file() is not True:
+        print("This is not a file. Aborting.")
+        sys.exit()
 
     home = Path.home()
-    repo = Path.join(home, "projects", "dotfiles", "unix", "")
+    repo = Path.joinpath(home, "projects", "dotfiles", "unix", "")
     cwd = Path.cwd()
     rel_path = Path.relative_to(cwd, home)
-    rel_file = Path.joinpath(rel_path, f)
+    rel_file = Path.joinpath(rel_path, src)
 
     main(rel_file)
