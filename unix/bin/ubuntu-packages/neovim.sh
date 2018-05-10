e99cbe2963166ca8558237ba7d5c5558ea62a505
farisachugthai <farischugthai@gmail.com>
2018-05-10 02:50:49 -0400

    Update script for setting up user with regular permissions and update neovim script.

--- unix/bin/ubuntu-packages/neovim.sh
+++ unix/bin/ubuntu-packages/neovim.sh
@@ -7,6 +7,4 @@ add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
 apt-get install neovim
 
-# Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+exit 0
