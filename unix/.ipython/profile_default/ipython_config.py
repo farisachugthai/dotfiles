761ad526966e38503c38370666218c0400ad9e56
farisachugthai <farischugthai@gmail.com>
2018-05-26 03:48:43 -0400

    Vim to Nvim transformation complete.

--- unix/.ipython/profile_default/ipython_config.py
+++ unix/.ipython/profile_default/ipython_config.py
@@ -295,7 +295,7 @@ c.TerminalInteractiveShell.display_completions = 'multicolumn'
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
-c.TerminalInteractiveShell.editor = 'vi'
+c.TerminalInteractiveShell.editor = 'nvim'
 
 ## Enable vi (v) or Emacs (C-X C-E) shortcuts to open an external editor. This is
 #  in addition to the F2 binding, which is always enabled.
