1b60ee8359d51f8f1891e31248ddf68d32f3c1d4
farisachugthai <farischugthai@gmail.com>
2018-07-15 10:55:33 -0400

    Configurations to i3wm and vim.
    Ensure dropbox autostarts after login. Worked flawlessly with kwin but
    having difficulties with i3.
    
    Then just continuing to trim down vim and ensure it can work as quickly
    as possible while still maintaining good integrations with other
    systems.

--- unix/.vim/skeleton.py
+++ unix/.vim/skeleton.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # Maintainer: Faris Chugthai
 
+# TODO: If you want throw in pep257 docstrings, module and func
+# Show some pep484 type hints
+# idk the stuff you always do but never remember how to do
+
 
 def main():
     """
